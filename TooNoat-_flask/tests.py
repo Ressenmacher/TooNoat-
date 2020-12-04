a69133a87ea48454398dc2f4ac0737585137411e
Noah Korzelius <korzeliusn@gmail.com>
2020-12-04 11:06:44 -0500

    Updated files for flask 06 assignment

--- TooNoat-_flask/tests.py
+++ TooNoat-_flask/tests.py
@@ -1,15 +1,13 @@
 import unittest
 import pip._vendor.requests as requests
 
-
 class FlaskTest(unittest.TestCase):
 
     def test_index(self):
-        response = requests.get("http://127.0.0.1:5000/")
+        response = requests.get("http://127.0.0.1:5000/index")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual(
-            '<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
 
     def test_notes(self):
         response = requests.get("http://127.0.0.1:5000/notes")
@@ -18,23 +16,21 @@ class FlaskTest(unittest.TestCase):
         self.assertEqual('Title' and 'Date' in response.text, True)
 
     def test_note(self):
-        response = requests.get("http://127.0.0.1:5000/notes/1")
+        response = requests.get("http://127.0.0.1:5000/notes/3")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('pp' in response.text, True)
+        self.assertEqual('First db Note' in response.text, True)
 
     def test_new(self):
         response = requests.get("http://127.0.0.1:5000/notes/new")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual(
-            '<form action="new" method="post">' in response.text, True)
+        self.assertEqual('<form action="new" method="post">' in response.text, True)
 
     def test_delete(self):
         response = requests.get('http://127.0.0.1:5000/notes/delete')
         statuscode = response.status_code
         self.assertEqual(statuscode, 500)
-
 
 if __name__ == " __main__":
     unittest.main()
